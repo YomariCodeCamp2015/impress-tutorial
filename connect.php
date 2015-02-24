f782c423cdfd19709fb062b8fa227fa31bd11dda
Sujit Maharjan <mail@maharjansujit.com.np>
2015-02-24 20:40:55 +0545

    sql not working

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = $OPENSHIFT_MYSQL_DB_URL;
+$servername = getenv('OPENSHIFT_MYSQL_DB_URL');
 $databasename = "impresstutorial";
-$username = $OPENSHIFT_MYSQL_DB_USERNAME;
-$password = $OPENSHIFT_MYSQL_DB_PASSWORD;
+$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
